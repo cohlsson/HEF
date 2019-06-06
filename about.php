e5fe3c0f7e00ccee1c5c1b321fac74ea442ac372
Chris Ohlsson <cohlsson55@gmail.com>
2019-06-06 15:45:36 -0400

    More changes to index and adding about.php

---
+++ about.php
@@ -0,0 +1,114 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title>Selden K. Smith Foundation for Holocaust Education</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Importing fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <header class="blog-header py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+          <div class="col-12 text-center">
+            <a class="blog-header-logo text-dark" href="index.php">The Selden K. Smith Foundation for Holocaust Education</a>
+          </div>
+        </div>
+      </header>
+
+      <div class="nav-scroller py-1 mb-2">
+        <nav class="nav d-flex justify-content-between">
+          <a class="p-2 text-muted" href="index.php">Home</a>
+          <a class="p-2 text-muted" href="about.php">About</a>
+          <a class="p-2 text-muted" href="board.php">Board</a>
+          <a class="p-2 text-muted" href="donation.php">Donate</a>
+          <!--<a class="p-2 text-muted" href="links.php">Links</a>-->
+          <a class="p-2 text-muted" href="contact.php">Contact</a>
+        </nav>
+      </div>
+    </div>
+
+<main role="main" class="container">
+  <div class="row">
+    <div class="col-md-8 blog-main">
+      <!--
+      <h3 class="pb-4 mb-4 font-italic border-bottom">
+        News
+      </h3>
+    -->
+      <div class="blog-post">
+        <h2 class="blog-post-title">Thank you!</h2>
+        <!--<p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>-->
+        <hr>
+        <img src="http://holocausteducationfoundation.org/images/thankyou.jpg" alt="Midlands Gives">
+        <p>The Selden K. Smith Foundation for Holocaust Education raised $7128 online during Midlands Gives and we still have checks coming in! We couldn't have done it without your support, so thank you all! We look forward to continuing our work around the state this year.</p>
+      </div><!-- /.blog-post -->
+
+      <div class="blog-post">
+        <h2 class="blog-post-title">Our community mourns the passing of Dr. Selden Smith</h2>
+        <!--<p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>-->
+        <hr>
+        <p>It is with much sadness and a heavy heart that I share the news of the passing of Dr. Selden K. Smith on February 12, 2018 at the age of 88.</p>
+        <p>Dr. Smith devoted much of his life to justice, tolerance and combatting hatred. He was truly the "father" of Holocaust Education in South Carolina. His tireless efforts to teach the lessons of the Holocaust have been an inspiration to many and his mentorship and guidance has influenced others to follow in his footsteps.</p>
+        <p><a href="http://holocausteducationfoundation.org/pdf/inmemoriam.pdf" title="Full Memoriam">READ OUR FULL MEMORIAM</a></p>
+        <p><a href="https://www.thestate.com/opinion/letters-to-the-editor/article200387779.html" title="The State">Read how Selden Smith tansformed Holocaust education in the Midlands</a></p>
+      </div><!-- /.blog-post -->
+
+    </div><!-- /.blog-main -->
+
+    <aside class="col-md-4 blog-sidebar">
+      <div class="p-4 mb-3 bg-light rounded">
+        <h4 class="font-italic">Mission Statement</h4>
+        <p class="mb-0">
+The Selden K. Smith Foundation for Holocaust Education was formed in May 2010 to further the programs of the South Carolina Council on the Holocaust. The Foundation functions as a private source of funding for support of Holocaust educational activities and community awareness events in South Carolina and has a 501 (c) (3) status.</p>
+      </div>
+
+      <div class="p-4">
+        <h4 class="font-italic">Links</h4>
+        <ol class="list-unstyled mb-0">
+          <li><a href="#">Columbia Holocaust Education Commission</a></li>
+          <li><a href="#">South Carolina Council on the Holocaust</a></li>
+          <li><a href="#">The United States Holocaust Memorial Museum</a></li>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Survivor/Libertor Tapes</a></li>
+        </ol>
+      </div>
+
+    </aside><!-- /.blog-sidebar -->
+
+  </div><!-- /.row -->
+
+</main><!-- /.container -->
+
+<footer class="blog-footer">
+  <p>The Selden K. Smith Foundation for Holocaust Education</p>
+</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="generator" content="">
-    <title>HEF</title>
+    <title>Selden K. Smith Foundation for Holocaust Education</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -105,68 +105,23 @@
         News
       </h3>
     -->
+      <div class="blog-post">
+        <h2 class="blog-post-title">Thank you!</h2>
+        <!--<p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>-->
+        <hr>
+        <img src="http://holocausteducationfoundation.org/images/thankyou.jpg" alt="Midlands Gives">
+        <p>The Selden K. Smith Foundation for Holocaust Education raised $7128 online during Midlands Gives and we still have checks coming in! We couldn't have done it without your support, so thank you all! We look forward to continuing our work around the state this year.</p>
+      </div><!-- /.blog-post -->
 
       <div class="blog-post">
         <h2 class="blog-post-title">Our community mourns the passing of Dr. Selden Smith</h2>
         <!--<p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>-->
         <hr>
-        <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-        <blockquote>
-          <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-        </blockquote>
-        <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-        <h2>Heading</h2>
-        <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-        <h3>Sub-heading</h3>
-        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        <pre><code>Example code block</code></pre>
-        <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-        <h3>Sub-heading</h3>
-        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <ul>
-          <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-          <li>Donec id elit non mi porta gravida at eget metus.</li>
-          <li>Nulla vitae elit libero, a pharetra augue.</li>
-        </ul>
-        <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-        <ol>
-          <li>Vestibulum id ligula porta felis euismod semper.</li>
-          <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-          <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-        </ol>
-        <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
+        <p>It is with much sadness and a heavy heart that I share the news of the passing of Dr. Selden K. Smith on February 12, 2018 at the age of 88.</p>
+        <p>Dr. Smith devoted much of his life to justice, tolerance and combatting hatred. He was truly the "father" of Holocaust Education in South Carolina. His tireless efforts to teach the lessons of the Holocaust have been an inspiration to many and his mentorship and guidance has influenced others to follow in his footsteps.</p>
+        <p><a href="http://holocausteducationfoundation.org/pdf/inmemoriam.pdf" title="Full Memoriam">READ OUR FULL MEMORIAM</a></p>
+        <p><a href="https://www.thestate.com/opinion/letters-to-the-editor/article200387779.html" title="The State">Read how Selden Smith tansformed Holocaust education in the Midlands</a></p>
       </div><!-- /.blog-post -->
-
-      <div class="blog-post">
-        <h2 class="blog-post-title">Another blog post</h2>
-        <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
-
-        <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-        <blockquote>
-          <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-        </blockquote>
-        <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-        <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-      </div><!-- /.blog-post -->
-
-      <div class="blog-post">
-        <h2 class="blog-post-title">New feature</h2>
-        <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
-
-        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <ul>
-          <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-          <li>Donec id elit non mi porta gravida at eget metus.</li>
-          <li>Nulla vitae elit libero, a pharetra augue.</li>
-        </ul>
-        <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-        <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-      </div><!-- /.blog-post -->
-
-      <nav class="blog-pagination">
-        <a class="btn btn-outline-primary" href="#">Older</a>
-        <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
-      </nav>
 
     </div><!-- /.blog-main -->
 
@@ -195,10 +150,7 @@ The Selden K. Smith Foundation for Holocaust Education was formed in May 2010 to
 </main><!-- /.container -->
 
 <footer class="blog-footer">
-  <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-  <p>
-    <a href="#">Back to top</a>
-  </p>
+  <p>The Selden K. Smith Foundation for Holocaust Education</p>
 </footer>
 </body>
 </html>
